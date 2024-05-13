2336009a4477e85c136bac065db40a74ea86562f
Guilherme <luizguilherme_54@hotmail.com>
2024-05-13 13:59:51 -0300

    Corrigindo erro de diretório

--- Aula5/src/Main.java
+++ Aula5/src/Main.java
@@ -1,7 +1,6 @@
-package resumo;
 
 public class Main {
 	public static void main(String[] args) {
-		Caneta c1 = new Caneta;
+		System.out.println("Hello Word! ");
 	}
 }
--- resumo/src/resumo/caneta.java
+++ resumo/src/resumo/caneta.java
@@ -1,16 +1,48 @@
-package resumo;
-
-public class caneta {
-
-	private double ponta;
+public class Caneta {
+	
+	
+	
 	private String marca;
+	private double ponta;
 	private int carga;
 	
 	
-	public caneta
+	public Caneta(String m, double p, int c) {
+		this.setMarca(m);
+		this.setPonta(p);
+		this.setCarga(c);
+	};
 	
-	public setPonta() {
-		this.setPonta(d);
+	
+	public void setMarca(String m) {
+		this.marca = m;
+	};
+	
+	public String getMarca() {
+		return this.marca;
+	}
+	
+	public void setPonta(double p) {
+		this.ponta = p;
+	}
+	
+	public double getPonta() {
+		return this.ponta;
+	}
+	
+	public void setCarga(int c) {
+		this.carga = c;
+	}
+	
+	public int getCarga() {
+		return this.carga;
+	}
+	
+	public void status() {
+		System.out.println("Marca = "+ this.marca );
+		System.out.println("Ponta = "+ this.ponta);
+		System.out.println("Carga = "+ this.carga );
+		
 	}
 	
 }
