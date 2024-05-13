00553739d3893274fc22d5295f0a9bfe2ca60c7e
Guilherme <luizguilherme_54@hotmail.com>
2024-05-13 14:43:21 -0300

    Atualizando o resumo

--- Aula5/src/Main.java
+++ Aula5/src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello Word! ");
+		
+		
+		Caneta c1 = new Caneta("BIC", 0.5, 90);
+		
+		
+		c1.status();
+		
 	}
 }
